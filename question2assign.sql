4195196500bee229643c903ae039057338ca7803
shinrl1 <shinrl1@student.op.ac.nz>
2019-09-23 10:45:06 +1200

    what doesn't kill you...

--- question2assign.sql
+++ question2assign.sql
@@ -19,7 +19,7 @@ exec createQoute @QouteID = 32, @QouteDescription = 'i am a description',@QouteD
 end
 
 
-alter proc addQouteComponent(@ComponentID int,@QouteID int, @Quantity int, @TimeToFit time)
+alter proc addQouteComponent(@ComponentID int,@QouteID int, @Quantity int)
 as -- because timetofit was a decimal and one data type I decided to forfit obtaining TimeToFit dynamically and thus added @TimeToFit as an input parameter.
 declare @ComponentID2 int
 set @ComponentID2 = @ComponentID
@@ -27,14 +27,19 @@ declare @TradePrice2 money
 set @TradePrice2 = (select TradePrice from Component where ComponentID = @ComponentID2 )
 declare @ListPrice2 money
 set @ListPrice2 = (select ListPrice from Component where ComponentID = @ComponentID2) 
+declare @TimeToFit2 decimal
+set @TimeToFit2 = (select TimeToFit from Component where ComponentID = @ComponentID2)
+
 insert into  QouteComponent (ComponentID, QouteID, Quantity, TradePrice, ListPrice, TimeToFit)
-values (@ComponentID, @QouteID, @Quantity, @TradePrice2, @ListPrice2, @TimeToFit) 
-go 
+values (@ComponentID, @QouteID, @Quantity, @TradePrice2, @ListPrice2, @TimeToFit2) 
+go	
 
 
 set identity_insert Qoute on --testing first sp
-exec createQoute @QouteID = 33, @QouteDescription = 'testing' , @QoutePrice = '$4', @QouteCompiler = 'Jenifer', @CustomerID = '23';
-exec addQouteComponent  @ComponentID =30906, @QouteID = 33, @Quantity = 100, @TimeToFit = '1:00'; 
+exec createQoute @QouteID = 13, @QouteDescription = 'testing' , @QoutePrice = '$4', @QouteCompiler = 'Jenifer', @CustomerID = '23';
+exec addQouteComponent  @ComponentID =30906, @QouteID = 33, @Quantity = 100; 
+
+select * from QouteComponent;
 
 select * from Qoute
 select * from QouteComponent
