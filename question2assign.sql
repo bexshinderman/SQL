f3f74936880383207866b05b8d8b4a5809753403
shinrl1 <shinrl1@student.op.ac.nz>
2019-09-22 21:18:29 +1200

    second question starting to make sense!

--- question2assign.sql
+++ question2assign.sql
@@ -17,16 +17,50 @@ set identity_insert Qoute on --testing first sp
 exec createQoute @QouteID = 32, @QouteDescription = 'i am a description',@QouteDate = null, @QoutePrice = '$4', @QouteCompiler = 'Jenifer', @CustomerID = '22';
 --adds to Qoute table as intended
 end
-
-alter proc addQouteComponent( -- second sp of weirdness
-							@ComponentID int,	
-							@QouteID int,
-							@Quantity int)
+--helper functions for second sp
+create function dbo.getTradePrice(@ComponentID2 int)
+returns money
 as
-insert into QouteComponent(ComponentID, QouteID, Quantity, TradePrice, ListPrice, TimeToFit)
-values ( @ComponentID, @QouteID, @Quantity, 
-'select TradePrice from Component where ComponentID = @ComponentID', 
-'select ListPrice from Component where ComponentID = @ComponentID' ,
-'select TradePrice from Component where TimeToFit = @ComponentID') -- how do I make these execute??
+begin
+return (select TradePrice from Component where ComponentID = @ComponentID2  )
+end
+go
+
+select dbo.getTradePrice (30901)
+
+create function dbo.getListPrice(@ComponentID2 int)
+returns money
+as
+begin
+return (select ListPrice from Component where ComponentID = @ComponentID2  )
+end
+go
+
+select dbo.getListPrice (30901)
+drop function dbo.getTimeToFit
+create function dbo.getTimeToFit(@ComponentID2 int)
+returns decimal
+as
+begin
+return (select TimeToFit from Component where ComponentID = @ComponentID2  ) 
+end
+go
+select * from component
+select dbo.getTimeToFit(30901)
+
+
+
+alter proc addQouteComponent(@ComponentID int,@QouteID int, @Quantity int, @TimeToFit time)
+as
+declare @ComponentID2 int
+set @ComponentID2 = @ComponentID
+declare @TradePrice2 money
+set @TradePrice2 = (select TradePrice from Component where ComponentID = @ComponentID2 )
+declare @ListPrice2 money
+set @ListPrice2 = (select ListPrice from Component where ComponentID = @ComponentID2) 
+insert into  QouteComponent (ComponentID, QouteID, Quantity, TradePrice, ListPrice, TimeToFit)
+values (@ComponentID, @QouteID, @Quantity, @TradePrice2, @ListPrice2, @TimeToFit) 
 go 
-exec addQouteComponent @QouteID = 32, @ComponentID =3091, @Quantity = 100; -- returns "Cannot convert a char value to money"
+exec addQouteComponent  @ComponentID =30911, @QouteID = 32, @Quantity = 100, @TimeToFit = '1:00'; -- returns "Cannot convert a char value to money"
+
+select * from QouteComponent;
